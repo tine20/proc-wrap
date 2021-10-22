fd30e80740fe76904d4c7be8e198d7de0db4eeeb
Paul Mehrer <p.mehrer@metaways.de>
2021-10-22 15:22:32 +0200

    add bsd-3 license and copyright

--- src/CmdException.php
+++ src/CmdException.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * This file is part of tine20/proc-wrap.
+ *
+ * (c) Metaways Infosystems GmbH (http://www.metaways.de)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 declare(strict_types=1);
 
